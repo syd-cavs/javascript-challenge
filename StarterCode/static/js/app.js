3649c9a194a3a485d73500b286594b7879021a0b
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-06-03 14:54:17 -0400

    finished level 1 during tutoring session

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,37 @@
 // from data.js
 var tableData = data;
-console.log(tableData);
+//console.log(tableData);
+var tbody = d3.select('tbody');
 
-var body = d3.select('tbody');
-tableData.forEach(function(ufo) {
-    console.log(ufo);
-    var row = body.append('tr');
-    Object.entries(ufo).forEach(([key, value]) => {
-        console.log(`${key}: ${value}`);
-        var cell = row.append('td');
-        cell.text(value);
-    });     
-});
+function createTable(x){
+    tbody.html('')
+    x.forEach(function(ufo) {
+        //console.log(ufo);
+        var row = tbody.append('tr');
+        Object.entries(ufo).forEach(([key, value]) => {
+            //console.log(`${key}: ${value}`);
+            var cell = row.append('td');
+            cell.text(value);
+        });     
+    });
+};
+
+function runEnter() {
+    
+
+    var filteredData = tableData;
+    var inputValue = d3.select('#datetime').property('value');
+    
+    filteredData = filteredData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+    createTable(filteredData);
+}
+
+d3.select("#filter-btn").on("click", runEnter);
+
+//d3.selectAll("input").on("change", runEnter);
+
+
+
+
+createTable(tableData);
