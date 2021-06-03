4662d74dfe7b862b41f5509f64b8123916b3e12c
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-06-03 12:39:31 -0400

    table uploaded

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,6 +4,11 @@ console.log(tableData);
 
 var body = d3.select('tbody');
 tableData.forEach(function(ufo) {
+    console.log(ufo);
     var row = body.append('tr');
-    Object.entries(ufo).forEach()
+    Object.entries(ufo).forEach(([key, value]) => {
+        console.log(`${key}: ${value}`);
+        var cell = row.append('td');
+        cell.text(value);
+    });     
 });
